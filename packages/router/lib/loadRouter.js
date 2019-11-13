d2126e71c71907382ddf0457f5ed664a17946d08
jammacn <jeff@jamma.cn>
2019-11-13 22:02:55 +0800

    loadRouter

--- packages/router/lib/loadRouter.js
+++ packages/router/lib/loadRouter.js
@@ -6,27 +6,39 @@ const log = require('jm-log4js')
 const logger = log.getLogger('main')
 const ms = new MS()
 
+function loadRouterFile (service, dir, file) {
+  const key = path.basename(file, '.js')
+  let prefix = `/${key}`
+  key === 'index' && (prefix = '/')
+  if (!fs.existsSync(`${dir}/${file}`) || !fs.statSync(`${dir}/${file}`).isFile()) return
+  const router = require(`${dir}/${key}`)(service)
+  router.prefix || (router.prefix = prefix)
+  logger.debug(`load router ${prefix}`)
+  return router
+}
+
 function loadRouter (service, dir) {
   const router = ms.router()
+
+  // 先处理 index.js
+  const r = loadRouterFile(service, dir, 'index.js')
+  r && (router.use(r.prefix, r))
+
   fs
     .readdirSync(dir)
     .filter(function (file) {
-      return (file.indexOf('.js') !== 0)
+      return (file.indexOf('.js') !== 0) && (file !== 'index.js')
     })
     .forEach(file => {
-      const key = path.basename(file, '.js')
-      let prefix = `/${key}`
-      file === 'index.js' && (prefix = '/')
       if (fs.statSync(`${dir}/${file}`).isDirectory()) {
+        const prefix = `/${file}`
         logger.debug(`load router dir ${prefix} ...`)
         router.use(prefix, loadRouter(service, `${dir}/${file}`))
         logger.debug(`load router dir ${prefix} ...ok`)
         return
       }
-      const r = require(`${dir}/${key}`)(service)
-      r.prefix && (prefix = r.prefix)
-      router.use(prefix, r)
-      logger.debug(`load router ${prefix}`)
+      const r = loadRouterFile(service, dir, file)
+      r && (router.use(r.prefix, r))
     })
   return router
 }
